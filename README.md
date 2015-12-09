88e09e08e227b183f0783958f78a73237adb3cdc
ÉtienneOz <salut@etienneozeray.fr>
2015-12-09 21:50:24 +0100

    tout doux read me

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # bekforum
 un forum pour troller.
+
+# Tout doux liste
+
+(ici)[https://lite6.framapad.org/p/toutdouxbekforum]
---
+++ messagesprives.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+$titre="Messages Privés";
+$balises = true;
+include("includes/identifiants.php");
+include("includes/debut.php");
+include("includes/bbcode.php");
+include("includes/menu.php");
+
+$action = (isset($_GET['action']))?htmlspecialchars($_GET['action']):'';
+
+
+$action = (isset($_GET['action']))?htmlspecialchars($_GET['action']):''; //On récupère la valeur de la variable $action
+
+switch($action)
+{
+  case "consulter": //1er cas : on veut lire un mp
+  //Ici on a besoin de la valeur de l'id du mp que l'on veut lire
+  break;
+
+  case "nouveau": //2eme cas : on veut poster un nouveau mp
+  //Ici on a besoin de la valeur d'aucune variable :p
+  break;
+
+  case "repondre": //3eme cas : on veut répondre à un mp reçu
+  //Ici on a besoin de la valeur de l'id du membre qui nous a posté un mp
+  break;
+
+  case "supprimer": //4eme cas : on veut supprimer un mp reçu
+  //Ici on a besoin de la valeur de l'id du mp à supprimer
+  break;
+
+  default; //Si rien n'est demandé ou s'il y a une erreur dans l'url, on affiche la boite de mp.
+
+} //fin du switch
+?>
